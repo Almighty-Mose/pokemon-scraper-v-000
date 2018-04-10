d433ad2b02a4c8c76448be34ebc6013081d35f15
Branden Mosley <branden.mosley1337@gmail.com>
2018-04-10 19:46:12 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,12 +1,12 @@
 class Pokemon
-  attr_accessor :id
   
   @@all = []
   
-  def initialize(name:, type:, db:)
+  def initialize(name:, type:, db:, id:)
     @name = name
     @type = type
     @db = db
+    @id = id
     @@all << self
   end
     
